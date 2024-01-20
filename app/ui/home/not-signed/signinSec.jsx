46a36f90cf8bc2dcd8174cd40d33c78e2018faf9
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-01-21 03:01:08 +0530

    Fixed signin and login button

--- app/ui/home/not-signed/signinSec.jsx
+++ app/ui/home/not-signed/signinSec.jsx
@@ -1,7 +1,7 @@
 import Glowbutton from "../../../util/glowbutton";
 
 export default function SigninSec() {
-    return(<div className="w-full flex flex-row justify-center items-center">
+    return(<div className="-mt-24 w-full flex flex-row justify-center items-center p-11">
         <Glowbutton name="Log In" link="/login" />
         <Glowbutton name="Sign Up" link="/signup" />
     </div>);
